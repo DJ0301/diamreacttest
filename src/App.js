0b1d199c887e35f9d822d88bd9b479c422ffca38
DJ0301 <dhananjayjoshi2003@gmail.com>
2024-07-01 23:59:36 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import AssetTrading from './Components/AssetTrading';
+function App() {
+  return (
+    <AssetTrading/>
+  );
+}
+
+export default App;
